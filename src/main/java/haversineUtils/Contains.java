a4aa64d6cf8a1871abec8c57fd83c725fb97a995
Benyuss <egokiraly@gmail.com>
2016-08-27 13:17:53 +0200

    that fkin egit tho.

--- src/main/java/haversineUtils/Contains.java
+++
@@ -1,7 +0,0 @@
-package haversineUtils;
-
-//It's better to return an enum rather than a Constant String. 
-//Used to know if the location given by user is contains the other locations or not.
-public enum Contains {
-	CONTAINS, NOT_CONTAINS, SEMI_CONTAINS;
-}
